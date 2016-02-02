daae590c9fd0d5ca742a2ec769c71578203d6af8
Eric VINCENT <ericvincenterv@gmail.com>
2016-02-02 16:59:56 +0100

    [WIP]fix #217 : travail sur l'évaluation

--- Form/DropzoneCommonType.php
+++ Form/DropzoneCommonType.php
@@ -94,6 +94,18 @@ class DropzoneCommonType extends AbstractType
                      'required' => false)
                       )
 
+            // EvaluationType. Ajout de cette zone, demande JJQ. InnovaERV
+            ->add('evaluationType', 'choice', array(
+                'choices' => array(
+                    'noEvaluation' => 'noEvaluation',
+                    'notation' => 'notation',
+                    'ratingScale' => 'ratingScale',
+
+                ),
+                'expanded' => true,
+                'multiple' => false,
+            ))
+
 
             ;
     }
