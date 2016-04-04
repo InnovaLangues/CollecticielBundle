59efb6d94707a18a069bb433f72d7fe1be4b70ae
Eric VINCENT <ericvincenterv@gmail.com>
2016-04-04 09:41:45 +0200

    fix claroline/Claroline#780 : add new tables for scale and criteria

---
+++ Entity/GradingCriteria.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by : Eric VINCENT
+ * Date: 04/2016
+ */
+
+namespace Innova\CollecticielBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="Innova\CollecticielBundle\Repository\GradingCriteriaRepository")
+ * @ORM\Table(name="innova_collecticielbundle_grading_criteria")
+ */
+class GradingCriteria {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="criteria_name", type="text", nullable=false)
+     */
+    protected $criteriaName;
+
+    /**
+     * Lien avec la table Dropzone
+    */
+    /**
+     * @ORM\ManyToOne(
+     *      targetEntity="Innova\CollecticielBundle\Entity\Dropzone"
+     * )
+     * @ORM\JoinColumn(name="dropzone_id", referencedColumnName="id", nullable=false)
+     */
+    protected $dropzone;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set criteriaName
+     *
+     * @param string $criteriaName
+     *
+     * @return GradingCriteria
+     */
+    public function setCriteriaName($criteriaName)
+    {
+        $this->criteriaName = $criteriaName;
+
+        return $this;
+    }
+
+    /**
+     * Get criteriaName
+     *
+     * @return string
+     */
+    public function getCriteriaName()
+    {
+        return $this->criteriaName;
+    }
+
+    /**
+     * Set dropzone
+     *
+     * @param \Innova\CollecticielBundle\Entity\Dropzone $dropzone
+     *
+     * @return GradingCriteria
+     */
+    public function setDropzone(\Innova\CollecticielBundle\Entity\Dropzone $dropzone)
+    {
+        $this->dropzone = $dropzone;
+
+        return $this;
+    }
+
+    /**
+     * Get dropzone
+     *
+     * @return \Innova\CollecticielBundle\Entity\Dropzone
+     */
+    public function getDropzone()
+    {
+        return $this->dropzone;
+    }
+}
---
+++ Entity/GradingScale.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by : Eric VINCENT
+ * Date: 04/2016
+ */
+
+namespace Innova\CollecticielBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="Innova\CollecticielBundle\Repository\GradingScaleRepository")
+ * @ORM\Table(name="innova_collecticielbundle_grading_scale")
+ */
+class GradingScale {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="scale_name", type="text", nullable=false)
+     */
+    protected $scaleName;
+
+    /**
+     * Lien avec la table Dropzone
+    */
+    /**
+     * @ORM\ManyToOne(
+     *      targetEntity="Innova\CollecticielBundle\Entity\Dropzone"
+     * )
+     * @ORM\JoinColumn(name="dropzone_id", referencedColumnName="id", nullable=false)
+     */
+    protected $dropzone;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set scaleName
+     *
+     * @param string $scaleName
+     *
+     * @return GradingScale
+     */
+    public function setScaleName($scaleName)
+    {
+        $this->scaleName = $scaleName;
+
+        return $this;
+    }
+
+    /**
+     * Get scaleName
+     *
+     * @return string
+     */
+    public function getScaleName()
+    {
+        return $this->scaleName;
+    }
+
+    /**
+     * Set dropzone
+     *
+     * @param \Innova\CollecticielBundle\Entity\Dropzone $dropzone
+     *
+     * @return GradingScale
+     */
+    public function setDropzone(\Innova\CollecticielBundle\Entity\Dropzone $dropzone)
+    {
+        $this->dropzone = $dropzone;
+
+        return $this;
+    }
+
+    /**
+     * Get dropzone
+     *
+     * @return \Innova\CollecticielBundle\Entity\Dropzone
+     */
+    public function getDropzone()
+    {
+        return $this->dropzone;
+    }
+}
